412ea5f14622ef5d7ecf19a1e9ee21c2d78ca9cf
PrincAm <ratbo90210@gmail.com>
2019-06-28 09:58:43 +0200

    add GTM for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,15 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: 'UA-121901485-2',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+      },
+    },
   ],
 }
